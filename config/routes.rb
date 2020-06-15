3e4346fff87609d19f985ad38cd65f4490041bdc
坪田健人 <lyla.0218@gmail.com>
2020-06-15 22:57:48 +0900

    全ファイル紐付け

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  namespace :admin do
+    resources :users
+  end
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  #get 'sessions/new'
+  root to:'tasks#index'
+  resources :tasks
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
