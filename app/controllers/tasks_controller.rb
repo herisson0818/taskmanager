8279986211114218ef91cc9ef5bb78e70b4e6d09
坪田健人 <lyla.0218@gmail.com>
2020-06-16 22:26:03 +0900

    destroyを修正

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -33,7 +33,7 @@ class TasksController < ApplicationController
 
   def destroy
     @task.destroy
-    redirect_to tasks_url, notice: "タスク「#{task.name}」を削除しました。"
+    redirect_to tasks_url, notice: "タスク「#{@task.name}」を削除しました。"
   end
 
   private
